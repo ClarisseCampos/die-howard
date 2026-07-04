4bfa9cf6be58a06cf7e1bc3e94eb2a043afeab97
Clarisse A Campos <tetearruda17@gmail.com>
2026-07-03 23:45:34 -0300

    Downloades em pasta determinada

--- yt-downloader/youtube.sh
+++ yt-downloader/youtube.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
 
-echo "Download"
-echo "1 - Video + audio"
-echo "2 - Audio only (mp3)"
+# Verify updates
+pipx upgrade yt-dlp >/dev/null 2>&1 || true
 
+# Define directory
+videofolder="/home/clarisse/yt-media/Video"
+audiofolder="/home/clarisse/yt-media/Audio"
+
+echo "YOUTUBE DOWNLOADER"
+
+echo -e "\n1 - Video + audio\n2 - Audio only (mp3)\n3 - Change download directory"
+
+echo
 read -p "Set option: " option
 read -p "Set URL: " url
 
 case $option in
     1) 
         yt-dlp \
+        -P "$videofolder" \
         -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" \
         --merge-output-format mp4 \
         "$url"
         ;;
     2)
         yt-dlp \
+        -P "$audiofolder" \
         -x \
         --audio-format mp3 \
         "$url"
