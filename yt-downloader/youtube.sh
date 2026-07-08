08aa96d7ff686df69cc465a5d3e72c145def6196
Clarisse A Campos <tetearruda17@gmail.com>
2026-07-08 20:29:42 -0300

    em portugues

--- yt-downloader/youtube.sh
+++ yt-downloader/youtube.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 
-# Define directory
-videofolder="$HOME/yt-media/Video"
-audiofolder="$HOME/yt-media/Audio"
+# Definir diretorios
+videofolder="$HOME/yt-media/video"
+audiofolder="$HOME/yt-media/audio"
 
-# Create directories 
+# Criar diretorios
 mkdir -p "$videofolder"
 mkdir -p "$audiofolder"
 
+# Menu
 echo "YOUTUBE DOWNLOADER"
-echo -e "\n1 - Video + audio\n2 - Audio only (mp3)"
+echo -e "\n1 - Video + Audio\n2 - Somente audio (mp3)"
 echo
-read -p "Set option: " option
-read -p "Set URL: " url
+read -p "Selecione a opcao: " option
+read -p "URL: " url
 
+# Controle
 case $option in
     1) 
+        # Audio + video (mp4)
         yt-dlp \
         -P "$videofolder" \
         -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/bestvideo+bestaudio" \
@@ -23,6 +26,7 @@ case $option in
         "$url"
         ;;
     2)
+        # Somente audio (mp3)
         yt-dlp \
         -P "$audiofolder" \
         -x \
@@ -30,6 +34,7 @@ case $option in
         "$url"
         ;;
     *)
-        echo "Invalid"
+        # opcao invalida
+        echo "Invalido"
         ;;
-esac 
+esac
